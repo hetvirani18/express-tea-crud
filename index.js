a7fc6bd070a6feff72d93075067267c795284651
Het Virani <hetvirani87@gmail.com>
2025-06-13 01:44:57 +0530

    implement logging with Winston and Morgan; add logger.js, update app.log,

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 
 app.use(express.json())
 
@@ -11,6 +32,7 @@ let nextId = 1;
 
 //add a new tea
 app.post('/teas', (req, res) => {
+    logger.info("post request received to add a new tea");
     const {name, price} = req.body
     const newTea = {id: nextId++, name, price}
     teaData.push(newTea);
@@ -19,11 +41,13 @@ app.post('/teas', (req, res) => {
 
 //get all tea
 app.get('/teas', (req, res) => {
+    logger.info("get request received to fetch all teas");
     res.status(200).send(teaData);
 })
 
 //get tea by id
 app.get('/teas/:id', (req, res) => {
+    logger.info(`get request received to fetch tea with id: ${req.params.id}`);
     const tea = teaData.find(t => t.id === parseInt(req.params.id))
     if(!tea){
         return res.status(404).send("404 tea not found");
@@ -33,6 +57,7 @@ app.get('/teas/:id', (req, res) => {
 
 //update tea
 app.put('/teas/:id', (req, res) => {
+    logger.info(`put request received to update tea with id: ${req.params.id}`);
     const tea = teaData.find(t => t.id === parseInt(req.params.id))
     if(!tea){
         return res.status(404).send("404 tea not found");
@@ -45,6 +70,7 @@ app.put('/teas/:id', (req, res) => {
 
 //delete tea
 app.delete('/teas/:id', (req, res) => {
+    logger.info(`delete request received to delete tea with id: ${req.params.id}`);
     const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
     if(index == -1){
         return res.status(404).send("404 tea not fount");
